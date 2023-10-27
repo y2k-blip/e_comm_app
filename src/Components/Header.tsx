0eb4cf4982bd26623a0bfb398c52efdd0f7c831a
Harsh Raut <59479702+Nerlozan21@users.noreply.github.com>
2023-10-27 16:19:10 +0530

    UI change for Header, Seach Bar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,15 +1,16 @@
 import "../Styles/HeaderStyles.css";
 import { useNavigate } from "react-router-dom";
 import SignIn from "./SignInCartBtn";
+import Search from "./Search";
 
 const Header = () => {
   const navigate = useNavigate();
   const navigateToHome = () => {
     navigate("/");
   };
-  const navigateToCategories = () => {
-    navigate("/categoriesListing");
-  };
+  // const navigateToCategories = () => {
+  //   navigate("/categoriesListing");
+  // };
 
   return (
     <header className="header">
@@ -20,6 +21,9 @@ const Header = () => {
           alt="Logo"
           className="logo"
         />
+      </div>
+      <div className="middle-section">
+        <Search></Search>
       </div>
       <div className="right-section">
         <SignIn></SignIn>
