31d3df1586955a9c8bf0a1a2474034804f81801a
Nerlozan <59479702+Nerlozan21@users.noreply.github.com>
2024-01-02 11:27:45 +0530

    Validation for email

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -8,17 +8,17 @@ const SignIn: React.FC = () => {
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
   };
-  // const validateEmail = () => {
-  //   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-  //   if (!emailRegex.test(email)) {
-  //     setError("Please enter a valid email address");
-  //   } else {
-  //     setError("");
-  //   }
-  // };
-  // useEffect(() => {
-  //   validateEmail();
-  // }, [email]);
+  const validateEmail = () => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address");
+    } else {
+      setError("");
+    }
+  };
+  useEffect(() => {
+    validateEmail();
+  }, [email]);
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
@@ -38,6 +38,7 @@ const SignIn: React.FC = () => {
             type="email"
             id="email"
             value={email}
+            onChange={handleEmailChange}
             required
           />
           {error && <div>{error}</div>}
